7d594371693fa52ff103e99322f894a8642d53a0
piyusss11 <piyusss11@gmail.com>
2024-05-31 13:12:39 +0530

    new

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: "1",
+      todo: "todo message",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (todo, id) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useTodo =()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
